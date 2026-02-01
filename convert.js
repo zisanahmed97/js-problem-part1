4ea60a4c8ca124448521fe8ec53f1d2690ae60f6
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-01 20:44:08 +0600

    added convert.js file

--- convert.js
+++ convert.js
@@ -4,3 +4,23 @@ function inchToFeet(inch){
 }
 const shuvoFeet = inchToFeet(75);
 console.log(shuvoFeet);
+
+
+function inchToFeet2(inch){
+    const feetFraction = inch/12;
+    const feetNumber = parseInt(feetFraction);
+    const inchRemaining = inch % 12;
+    const result = feetNumber + ' feet '+ inchRemaining+' inch ';
+    return result;
+}
+
+const shuvoFeet2 = inchToFeet2(75);
+console.log(shuvoFeet2);
+
+
+function kmToMile(km){
+    const mile = km * 0.621371;
+    return mile;
+}
+const result = kmToMile(3);
+console.log(result);
