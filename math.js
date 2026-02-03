35dfab0fcb5fba775293ac44b34e9719b2986028
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-03 23:05:31 +0600

    added math file

---
+++ math.js
@@ -0,0 +1,16 @@
+const min = Math.min(15,67,-19,90,365,0,-45);
+console.log(min);
+
+const max = Math.max(15,67,-19,90,365,0,-45);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(5-10)); // abs negative value kew positive dekhay
+console.log(Math.round(2.76)); // round kono vognangsho wala value ke purnoshongkha kore dibe
+console.log(Math.floor(2.96)); // eta dile niche neme jabe like 2.96 hye jabe 2
+console.log(Math.ceil(2.1)); // eta dile upore uthe jabe like 2.1 hye jabe 3
+
+console.log(Math.random()); // eta 0-9 er moddhe ekta random fraction value dibe
+console.log(Math.random()*10); // 10 diye multiply korbe
+
+console.log(Math.round(Math.random()*10));//eta dile purno shongkha dibe
